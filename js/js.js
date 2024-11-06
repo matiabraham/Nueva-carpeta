1739f91f2fce96e7411f42d7acb739915c32cbc9
matiasabraham <matiasabrahammosconi@gmail.com>
2024-11-05 22:24:06 -0300

    la pobre entrega no terminada

---
+++ js/js.js
@@ -0,0 +1,12 @@
+//funciones
+function productoYprecio () {
+    let nombreProducto = prompt("ingrese el producto:");
+    let precioProducto = parseFloat(prompt("ingrese el precio del producto;"));
+    if (isNaN(precioProducto) || precioProducto < 0);
+    alert("ingrese un precio valido");
+    return 0;
+}
+return precio;
+
+
+//inicio del programa
