b61fa1dff19f63b692ed88f9ff159a36ae09e28b
matiasabraham <matiasabrahammosconi@gmail.com>
2024-11-25 20:39:38 -0300

    hice unas cositas pero no tenia tiempo

--- js/js.js
+++ js/js.js
@@ -1,5 +1,10 @@
 //Objetos
 
 //funciones
+const sumarNotas = (nota1, nota2, nota3) => (nota1 + nota2 + nota3) / 3
+
 
 //inicio del programa
+
+let ingresar = prompt(" 1: Ingrese sus notas" + " 2: Calcular su nota final" + " 3: Verificar si aprobo")
+
